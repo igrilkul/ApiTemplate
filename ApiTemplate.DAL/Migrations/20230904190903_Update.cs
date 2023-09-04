dbd10c900d3c628115e99bce8e08d97e2306fd95
igrilkul <igrilkul@hotmail.com>
2023-09-04 22:15:49 +0300

    Changed Class to Enum

---
+++ ApiTemplate.DAL/Migrations/20230904190903_Update.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TextualRPG.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ClassName",
+                table: "Characters",
+                type: "int",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ClassName",
+                table: "Characters",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldMaxLength: 50);
+        }
+    }
+}
--- ApiTemplate.DAL/Models/Character.cs
+++ ApiTemplate.DAL/Models/Character.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TextualRPG.DAL.Enums;
 
 namespace TextualRPG.DAL.Models
 {
@@ -11,22 +12,21 @@ namespace TextualRPG.DAL.Models
         public int Id { get; private set; }
         public string Name { get; private set; } = string.Empty;
         public int Level { get; private set; } = 0;
-        public string ClassName { get; private set; } = string.Empty;
+        public ClassType ClassName { get; private set; } = 0;
 
         public string FullText => $"Lv{Level} {Name}";
 
         // not mapped
         private Character() { }
 
-        public Character(int id, string name, int level, string className)
+        public Character( string name, int level, ClassType className)
         {
-            Id = id;
             Name = name;
             Level = level;
             ClassName = className;
         }
 
-        public void UpdateCharacter(string name, int level, string className)
+        public void UpdateCharacter(string name, int level, ClassType className)
         {
             Name = name;
             Level = level;
