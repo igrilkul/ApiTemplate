e5475a101c38dfb5efb7cfbaa2435c21f42b3db0
igrilkul <igrilkul@hotmail.com>
2023-10-18 15:01:48 +0300

    Added Composite pattern for skills

--- ApiTemplate.DAL/Models/Skill.cs
+++ ApiTemplate.DAL/Models/Skill.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TextualRPG.DAL.Contracts;
 using TextualRPG.DAL.Enums;
 
 namespace TextualRPG.DAL.Models
 {
-    public class Skill
+    public class Skill : ISkill
     {
+        private readonly List<ISkill>? _children;
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public int RequiredLevel { get; set; } = 0;
@@ -30,5 +32,34 @@ namespace TextualRPG.DAL.Models
         public bool HasMagicDamage { get; set; } = false;
         public string LevelScaling { get; set; } = string.Empty;
 
+        private Skill() {}
+
+        public Skill(string name, int requiredLevel, string description, int mpUsage, int staminaUsage, int cooldown, SkillType skillType)
+        {
+            Name = name;
+            RequiredLevel = requiredLevel;
+            Description = description;
+            MpUsage = mpUsage;
+            StaminaUsage = staminaUsage;
+            Cooldown = cooldown;
+            SkillType = skillType;
+            _children = new List<ISkill>();
+        }
+
+        public void AddChild(ISkill child)
+        {
+            _children.Add(child);
+        }
+
+        public void DisplayInfo(string indent = "")
+        {
+            Console.WriteLine($"{indent}Skill: {Name}");
+            Console.WriteLine($"{indent}Description: {Description}");
+
+            foreach (var child in _children)
+            {
+                child.DisplayInfo(indent + "  ");
+            }
+        }
     }
 }
---
+++ TextualRPG.DAL.Contracts/ISkill.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextualRPG.DAL.Contracts
+{
+    public interface ISkill
+    {
+        void DisplayInfo(string indent = "");
+    }
+}
